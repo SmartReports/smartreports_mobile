12fe449e72d6518aca7ff88d4ab027b06ea0db7b
Filippo Biondi <f.biondi12@studenti.unipi.it>
2024-01-12 20:37:56 +0100

    changed webview library, disabled zoom and scrollbars

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,162 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: WebViewApp(),
-    ),
-  );
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isAndroid) {
+    await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
+
+  runApp(MaterialApp(
+      home: new MyApp()
+  ));
 }
 
-class WebViewApp extends StatefulWidget {
-  const WebViewApp({super.key});
-
+class MyApp extends StatefulWidget {
   @override
-  State<WebViewApp> createState() => _WebViewAppState();
+  _MyAppState createState() => new _MyAppState();
 }
 
-class _WebViewAppState extends State<WebViewApp> {
-  late final WebViewController controller;
+class _MyAppState extends State<MyApp> {
+
+  final GlobalKey webViewKey = GlobalKey();
+
+  InAppWebViewController? webViewController;
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+        useShouldOverrideUrlLoading: true,
+        mediaPlaybackRequiresUserGesture: false,
+        verticalScrollBarEnabled: false,
+        horizontalScrollBarEnabled: false,
+        disableHorizontalScroll: true,
+        supportZoom: false,
+      ),
+      android: AndroidInAppWebViewOptions(
+        useHybridComposition: true,
+        builtInZoomControls: false,
+      ),
+      ios: IOSInAppWebViewOptions(
+        allowsInlineMediaPlayback: true,
+
+      )
+  );
+
+  late PullToRefreshController pullToRefreshController;
+  String url = "";
+  double progress = 0;
+  final urlController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(
-        Uri.parse('https://smartreports.it'),
-      );
+
+    pullToRefreshController = PullToRefreshController(
+      options: PullToRefreshOptions(
+        color: Colors.blue,
+      ),
+      onRefresh: () async {
+        if (Platform.isAndroid) {
+          webViewController?.reload();
+        } else if (Platform.isIOS) {
+          webViewController?.loadUrl(
+              urlRequest: URLRequest(url: await webViewController?.getUrl()));
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter WebView'),
-      ),
-      body: WebViewWidget(
-        controller: controller,
-      ),
+      body: SafeArea(
+        child: WillPopScope(
+          onWillPop: () async {
+            final controller = webViewController;
+            if(controller != null) {
+              if (await controller.canGoBack()) {
+                await controller.goBack();
+                return false;
+              }
+            }
+            return true;
+        },
+          child: InAppWebView(
+          key: webViewKey,
+          initialUrlRequest:
+          URLRequest(url: Uri.parse("https://smartreports.it/")),
+          initialOptions: options,
+          pullToRefreshController: pullToRefreshController,
+          onWebViewCreated: (controller) {
+            webViewController = controller;
+          },
+          onLoadStart: (controller, url) {
+            setState(() {
+              this.url = url.toString();
+              urlController.text = this.url;
+            });
+          },
+          androidOnPermissionRequest: (controller, origin, resources) async {
+            return PermissionRequestResponse(
+                resources: resources,
+                action: PermissionRequestResponseAction.GRANT);
+          },
+          shouldOverrideUrlLoading: (controller, navigationAction) async {
+            var uri = navigationAction.request.url!;
+
+            if (![ "http", "https", "file", "chrome",
+              "data", "javascript", "about"].contains(uri.scheme)) {
+              if (await canLaunch(url)) {
+                // Launch the App
+                await launch(
+                  url,
+                );
+                // and cancel the request
+                return NavigationActionPolicy.CANCEL;
+              }
+            }
+
+            return NavigationActionPolicy.ALLOW;
+          },
+          onLoadStop: (controller, url) async {
+            pullToRefreshController.endRefreshing();
+            setState(() {
+              this.url = url.toString();
+              urlController.text = this.url;
+            });
+          },
+          onLoadError: (controller, url, code, message) {
+            pullToRefreshController.endRefreshing();
+          },
+          onProgressChanged: (controller, progress) {
+            if (progress == 100) {
+              pullToRefreshController.endRefreshing();
+            }
+            setState(() {
+              this.progress = progress / 100;
+              urlController.text = this.url;
+            });
+          },
+          onUpdateVisitedHistory: (controller, url, androidIsReload) {
+            setState(() {
+              this.url = url.toString();
+              urlController.text = this.url;
+            });
+          },
+          onConsoleMessage: (controller, consoleMessage) {
+            print(consoleMessage);
+          },
+        ),
+        ),
+        ),
     );
   }
 }
